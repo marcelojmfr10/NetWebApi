dcc715e21473d112894265dc135fe9be6fef1659
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-07-27 20:53:18 -0600

    Add Curso entity properties and seed data in DbContext

--- src/MasterNet.Persistence/MasterNetDbContext.cs
+++ src/MasterNet.Persistence/MasterNetDbContext.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using MasterNet.Domain;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,11 @@ namespace MasterNet.Persistence;
 
 class MasterNetDbContext : DbContext
 {
+    public DbSet<Curso>? Cursos { get; set; }
+    public DbSet<Instructor>? Instructores { get; set; }
+    public DbSet<Precio>? Precios { get; set; }
+    public DbSet<Calificacion>? Calificaciones { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=LocalDatabase.db")
@@ -53,5 +59,51 @@ class MasterNetDbContext : DbContext
                 j.HasKey(t => new { t.InstructorId, t.CursoId });
             }
         );
+
+        modelBuilder.Entity<Curso>().HasData(DataMaster().Item1);
+        modelBuilder.Entity<Precio>().HasData(DataMaster().Item2);
+        modelBuilder.Entity<Instructor>().HasData(DataMaster().Item3);
+    }
+
+    public Tuple<Curso[], Precio[], Instructor[]> DataMaster()
+    {
+        var cursos = new List<Curso>();
+        var faker = new Faker();
+
+        for (int i = 1; i < 10; i++)
+        {
+            var cursoId = Guid.NewGuid();
+            cursos.Add(new Curso
+            {
+                Id = cursoId,
+                Descripcion = faker.Commerce.ProductDescription(),
+                Titulo = faker.Commerce.ProductName(),
+                FechaPublicacion = DateTime.UtcNow
+            });
+        }
+
+        var precioId = Guid.NewGuid();
+        var precio = new Precio
+        {
+            Id = precioId,
+            PrecioActual = 10.0m,
+            PrecioPromocion = 8.0m,
+            Nombre = "Precio Regular"
+        };
+
+        var precios = new List<Precio>
+        {
+            precio
+        };
+
+        var fakerInstructor = new Faker<Instructor>()
+        .RuleFor(t => t.Id, _ => Guid.NewGuid())
+        .RuleFor(t => t.Nombre, f => f.Name.FirstName())
+        .RuleFor(t => t.Apellidos, f => f.Name.LastName())
+        .RuleFor(t => t.Grado, f => f.Name.JobTitle());
+
+        var instructores = fakerInstructor.Generate(10);
+
+        return Tuple.Create(cursos.ToArray(), precios.ToArray(), instructores.ToArray());
     }
 }
